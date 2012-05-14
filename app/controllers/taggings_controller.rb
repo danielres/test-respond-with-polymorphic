08e05bcc788682f276579c2b9efaf43874dd3060
Daniel Reszka <draens@hotmail.com>
2012-05-14 14:27:32 +0200

    select2 working for tag assignements (remote data loading via ajax/json not yet implemented)

--- app/controllers/taggings_controller.rb
+++ app/controllers/taggings_controller.rb
@@ -1,13 +1,16 @@
 class TaggingsController < ApplicationController
+  respond_to :html, :json
 
   def create
-    @tag = Tag.find_or_create_by_name(params[:tagging][:tag][:name])
-    @taggable = class_eval(params[:tagging][:taggable_type]).find(params[:tagging][:taggable_id])
-    begin
-      @taggable.tags << @tag
-      flash[:notice] = "Tagging was created successfully."
-    rescue
-      flash[:error]  = "Could not create this tag association, maybe it was already existing ?"
+    params[:tagging][:tag][:name].split(",").each do |n|
+      @tag = Tag.find_or_create_by_name(n)
+      @taggable = class_eval(params[:tagging][:taggable_type]).find(params[:tagging][:taggable_id])
+      begin
+        @taggable.tags << @tag
+        flash[:notice] = "Tagging was created successfully (#{@tag.name})."
+      rescue
+        flash[:error]  = "Could not create this tag association (#{@tag.name}), maybe it was already existing ?"
+      end
     end
     redirect_to :back
   end
@@ -16,6 +19,11 @@ class TaggingsController < ApplicationController
     tagging = Tagging.find(params[:id])
     flash[:notice] = "Tagging was removed successfully." if tagging.destroy
     redirect_to :back
+  end
+
+  def index
+    @taggings = Tag.where("name like ?", "%#{params[:q]}%")
+      respond_with(@taggings)
   end
 
 end
